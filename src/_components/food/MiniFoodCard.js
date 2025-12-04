0d0b639b77e8a10e5efce69cb8c03a22577d9480
25LP5105 25LP5105 <25LP5105@G302-CC0181.local>
2025-12-04 12:33:14 +0800

    m

--- src/_components/food/MiniFoodCard.js
+++ src/_components/food/MiniFoodCard.js
@@ -1,18 +1,19 @@
 "use client";
 
-export default function MiniFoodCard({image, name, price, description}) {
+import Image from "next/image";
+
+export default function MiniFoodCard({ image, name, price, description }) {
   return (
     <div className="bg-white border border-neutral-200  w-[270.75px] h-[241px] rounded-xl flex flex-col">
-      <div className="flex justify-center pt-[16px]">
-        {" "}
-        <img src={image} width={238.75} height={129} />{" "}
-      </div >
-      <div className="pt-[20px] pl-[15px] pr-[16px]">
-      <div className=" flex flex-row justify-between ">
-        < p className="text-red-500 text-sm "> {name} </p>
-        <p className="text-xs">${price}</p>
+      <div className="flex justify-center mt-[16px] ml-[16px] w-60 h-[135px] relative">
+        <Image src={image} fill className="object-cover" />
       </div>
-      
+      <div className="pt-[20px] pl-[15px] pr-[16px]">
+        <div className=" flex flex-row justify-between ">
+          <p className="text-red-500 text-sm "> {name} </p>
+          <p className="text-xs">${price}</p>
+        </div>
+
         <p className="text-sm text-gray-600 mt-1">{description}</p>
       </div>
     </div>
